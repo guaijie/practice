f206a1b073ee5b5a962356b6ab3d0bfcea05f60d
guaijie <1427392095xlk@gmail.com>
2018-11-30 16:20:40 +0800

    upload file

---
+++ canvas/README.md
@@ -0,0 +1,5 @@
+ #  canvas
+
+##  canvas 画图模糊问题
+
+ >  使用style将canvas设定成指定的宽高，使用canvas的属性width和height将宽高设成style宽高的两倍，同时在绘画时将所有数据放大到两倍即可。
--- webWorker/webWorker.js
+++ webWorker/webWorker.js
@@ -1,6 +1,11 @@
 self.onmessage = e => {
   console.log('主线程传来的信息：', e.data);
-  // do something
+  // console.log($)
+  // console.log(e);
 };
+
+// self.addEventListener('data',function(){
+//   console.log('data')
+// })
 
 self.postMessage('来自worker线程的消息');
